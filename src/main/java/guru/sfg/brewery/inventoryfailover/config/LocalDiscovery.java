49a2c8bf0641ca1bb46cfb8ef8a8f31ce91a15ed
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-08-09 18:00:24 +0200

    Initial project

---
+++ src/main/java/guru/sfg/brewery/inventoryfailover/config/LocalDiscovery.java
@@ -0,0 +1,9 @@
+package guru.sfg.brewery.inventoryfailover.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@EnableDiscoveryClient
+public class LocalDiscovery {
+}
